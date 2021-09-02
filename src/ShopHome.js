8117ca0b201934006b4985fbf80b42cd86a5b8c0
Rajesh <rajeshmurugesan228@gmail.com>
2021-09-02 22:01:10 +0530

    initial commit

---
+++ src/ShopHome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/Header";
+import Product from "./Product/Product";
+import Footer from "./Footer/Footer";
+
+export default function ShopHome() {
+  return (
+    <div>
+      <Header />
+      <Product />
+      <Footer />
+    </div>
+  );
+}
